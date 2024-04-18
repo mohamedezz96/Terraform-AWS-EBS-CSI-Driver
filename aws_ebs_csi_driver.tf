4c262a97d49a140b8cdbefbd0d1122ff0e74d1fa
mohamed ezz <mohamed.elsayedezzeldin@vodafone.com>
2024-04-18 12:26:16 +0200

    adding values.yaml

--- aws_ebs_csi_driver.tf
+++ aws_ebs_csi_driver.tf
@@ -2,5 +2,5 @@ module "aws_ebs_csi_driver" {
   source                              = "github.com/mohamedezz96/Terraform-Modules/EKS-Tools/AWS-EBS-CSI-Driver"
   aws_efs_csi_driver_version          = "2.29.1"
   eks_issuer                          = data.aws_eks_cluster.eks_data.identity[0].oidc[0].issuer
-  values_file                         = "./values/aws_ebs_csi_driver.yaml"
+  values_file                         = "./aws_ebs_csi_driver.yaml"
 }
