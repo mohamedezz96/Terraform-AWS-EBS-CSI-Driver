988e6b62bee7cd33869be1f3f5d315b6efb23b1e
mohamedezz96 <100701600+mohamedezz96@users.noreply.github.com>
2024-04-18 12:16:25 +0200

    Update aws_ebs_csi_driver.tf

--- aws_ebs_csi_driver.tf
+++ aws_ebs_csi_driver.tf
@@ -2,7 +2,5 @@ module "aws_ebs_csi_driver" {
   source                              = "github.com/mohamedezz96/Terraform-Modules/EKS-Tools/AWS-EBS-CSI-Driver"
   aws_efs_csi_driver_version          = "2.29.1"
   eks_issuer                          = data.aws_eks_cluster.eks_data.identity[0].oidc[0].issuer
-  aws_efs_controller_sa_name          = "aws-ebs-controller-sa"
-  aws_efs_node_sa_name                = "aws-ebs-node-sa"
   values_file                         = "./values/aws_ebs_csi_driver.yaml"
 }
